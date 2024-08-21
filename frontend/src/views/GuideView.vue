6f9c89bbc326225131054c6474881a6745706b9b
sohankolla <sohan.kolla03@gmail.com>
2024-08-20 20:34:46 -0400

    chore<frontend>: Updated Guide page to current information and made minor adjustments to Registration page

--- frontend/src/views/GuideView.vue
+++ frontend/src/views/GuideView.vue
@@ -3,49 +3,95 @@
 </script>
 
 <template>
-    <div class="font-sans px-4 py-8 text-cl5 bg-cl2 rounded m-5">
-        <h2 id="tps-will-soon-update-to-3906" class="text-2xl font-bold mb-4">EGTS WILL SOON UPDATE TO 39.06</h2>
-        <h2 id="tps-setup---connecting-and-using-tps-which-is-a-public-chn-server" class="text-2xl font-bold mb-4">EGTS
-            SETUP - Connecting and using EGTS, which is a public CHN server</h2>
-        <p class="mb-4"><strong>TO USE TPS, YOU MUST DOWNLOAD THE TPS PACK FROM THE LINK DOWN BELOW.</strong><br/>
-            <a href="https://canary.discord.com/channels/859872100860035113/859878646738583553/1204233371077709824"
-               class="text-cl6 hover:underline">https://canary.discord.com/channels/859872100860035113/859878646738583553/1204233371077709824</a><br/>
-            <br/>If you are unable to access this link, make sure you are in the Taiko Modding Discord server and have access
-            to
-            the #mods-and-customs channel. <br/> You can get the invite link at the bottom of this guide.
-            <strong>If you are downloading for the first time, use the full download. The quick download is only for
-                updating outdated versions!</strong></p>
-        <p class="mb-4"><strong>If you don&#39;t want to download the TPS PACK or you don&#39;t want to connect to TPS
-            you can setup your
-            own local sever using TLS. <br/>Instruction on how to set that up are in the EXTRA section where it says &quot;TLS
-            SETUP&quot;.</strong></p>
-        <p class="mb-4"><strong>For support with TPS or questions and suggestions join the TPS discord server: </strong>
-            <strong><a href="https://discord.gg/GT4jmqQD7M" class="text-cl6 hover:underline">https://discord.gg/GT4jmqQD7M</a></strong>
-        </p>
-        <p class="mb-4">Installing TPS for the first time:
-            Extract TPS.zip into the folder where you extracted CHN. (E.G. C:\TaikoNijiiro)
-            When asked to replace files, make sure you choose "REPLACE FILES". If you did not get asked to replace
-            files you did something wrong.</p>
-        <p class="mb-4">Updating TPS to a newer version:
-            Follow the same procedures as if you were installing TPS for the first time, this time using the Quick
-            Download.
-            If the Quick Download is not available or if your version is too old to support the quick download (if the
-            date
-            of your current version is not between the dates of the updates it contains), use the Full Download instead.
-            You will be able to tell if your current version is outdated if the game tells your that you are using an
-            outdated client in the song select.</p>
-        <p class="mb-4">Open config.toml located in Executable\Release_China with notepad and change <strong>server =
-            "127.0.0.1"</strong>
-            to <strong>server = "tps.keitannunes.ca"</strong></p>
-        <p class="mb-4">After you play 1 credit, you can register your card in the WebUI.
-            TPS WebUI: <a href="http://tps.keitannunes.ca:5000/" class="text-cl6 hover:underline">http://tps.keitannunes.ca:5000/</a>
-        </p>
-        <p class="mb-4">TPS Discord:
-            <a href="https://discord.gg/GT4jmqQD7M" class="text-cl6 hover:underline">https://discord.gg/GT4jmqQD7M</a>
-        </p>
-        <p>Taiko no Tatsujin Modding Discord Server:
-            <a href="https://discord.gg/HFm37aA5zr" class="text-cl6 hover:underline">https://discord.gg/HFm37aA5zr</a>
-        </p>
+    <div class="font-sans py-8 text-gray-200 bg-cl2 rounded my-5 mx-52 px-10">
+      <h2 id="egts-setup---connecting-and-using-egts-which-is-a-public-chn-server" class="text-center underline text-2xl font-bold mb-4">
+        EGTS SETUP - Connecting and using EGTS, which is a public JP/39.06 server
+      </h2>
+      <p class="mb-4">
+        <strong class="underline">HOW TO MIGRATE FROM CHN TO 39.06</strong>
+        <br/>
+        Copy your access code from the EGTS webui <strong>WITHOUT</strong> the BNTTCNID
+        <br/>
+        Inside card.ini(39.06) replace accessCode1= with your card number (E.G. accessCode1=42429075045032907255)
+        <br/>
+        Repeat for accessCode2 <strong>IF</strong> needed
+      </p>
+      <p class="mb-4">
+        <strong class="underline">TO USE EGTS, YOU MUST DOWNLOAD THE EGTS PACK FROM THE LINK DOWN BELOW.</strong>
+        <br/>
+        <a href="https://discord.com/channels/1206717593893740658/1246188695326035970/1249188290016579624"
+             class="text-cl6 hover:underline">
+          https://discord.com/channels/1206717593893740658/1246188695326035970/1249188290016579624
+        </a>
+        <br/>
+        If you are unable to access this link, make sure you are in the EGTS Discord server. You can get the invite link at the bottom of this guide.
+        <br/>
+        <strong>
+          If you are downloading for the first time, use the full download. The quick download is only for
+          updating outdated versions!
+        </strong>
+      </p>
+      <p class="mb-4">
+        <strong>
+          If you don&#39;t want to download the EGTS PACK or you don&#39;t want to connect to EGTS, you can setup your
+          own local server using TLS.
+          <br/>
+          Instructions on how to set that up are in the EXTRA section where it says "TLS SETUP"
+          in the Nijiiro setup guide.
+        </strong>
+      </p>
+      <p class="mb-4">
+        <strong class="underline">For support with EGTS, or questions and suggestions, join the EGTS discord server:</strong>
+        <br/>
+        <strong>
+          <a href="https://discord.gg/egts" class="text-cl6 hover:underline">https://discord.gg/egts</a>
+        </strong>
+      </p>
+      <p class="mb-4">
+        <strong class="underline">Installing EGTS for the first time:</strong>
+        <br/>
+        Extract EGTSFullDownload.zip into the folder where you extracted 39.06. (E.G. C:\TaikoNijiiro)
+        <br/>
+        When asked to replace files, make sure you choose <strong>"REPLACE FILES"</strong>.
+        <br/>
+        If you did not get asked to replace files you did something wrong.
+      </p>
+      <p class="mb-4">
+        <strong class="underline">Updating EGTS to a newer version:</strong>
+        <br/>
+        Follow the same procedures as if you were installing EGTS for the first time, this time using the Quick
+        Download.
+        <br/>
+        If the Quick Download is not available or if your version is too old to support the quick download (if the
+        date of your current version is not between the dates of the updates it contains), use the Full Download instead.
+        <br/>
+        You will be able to tell if your current version is outdated if the game tells you that you are using an
+        outdated client in the song select.
+      </p>
+      <p class="mb-4">
+        Open config.toml located in Executable\ with notepad and change <strong>server = "127.0.0.1"</strong>
+        to <strong>server = "<a class="text-cl6 hover:underline" href="https://egts.ca/">egts.ca</a>"</strong>
+        <br/>
+        <br/>
+        <strong>TAIKO MIGHT TAKE EXTRA LONG TO LOAD WHEN LOADING FOR THE FIRST TIME. (~90s)</strong>
+      </p>
+      <p class="mb-4">
+        After you play 1 credit, you can register your card in the WebUI.
+        <br/>
+        EGTS WebUI:
+        <br/>
+        <a href="https://egts.ca/" class="text-cl6 hover:underline">https://egts.ca/</a>
+      </p>
+      <p class="mb-4">
+        EGTS Discord:
+        <br/>
+        <a href="https://discord.gg/egts" class="text-cl6 hover:underline">https://discord.gg/egts</a>
+      </p>
+      <p>
+        Taiko no Tatsujin Modding Discord Server:
+        <br/>
+        <a href="https://discord.gg/HFm37aA5zr" class="text-cl6 hover:underline">https://discord.gg/HFm37aA5zr</a>
+      </p>
     </div>
 </template>
 
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -13,11 +13,11 @@ if (authStore.isAuthenticated) {
     <div class="flex flex-col items-center justify-center px-6 py-5 mx-auto md:my-16 lg:py-0">
         <div class="flex flex-col items-center space-y-5 mb-6 text-4xl">
             <img class="w-20 h-20 mr-2" src="../assets/taiko.png" alt="logo"/>
-            <h1 class="font-[taiko] text-center">Taiko Public Server</h1>
+            <h1 class="font-[taiko] text-center">Elara Global Taiko Server</h1>
         </div>
         <div class="w-full bg-cl2 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8 ">
-                <h1 class="text-3xl center-self leading-tight tracking-tight text-cl5 text-bold">
+                <h1 class="text-3xl font-bold center-self leading-tight tracking-tight text-cl5 text-bold">
                     Register
                 </h1>
                 <div class="space-y-4">
