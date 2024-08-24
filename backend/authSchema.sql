4903d13b0b30165a4c2b4ec25864cb9103c7c50d
sohankolla <sohan.kolla03@gmail.com>
2024-08-24 04:20:51 -0400

    fix<backend>: set default value for customTitleOn to true

--- backend/authSchema.sql
+++ backend/authSchema.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users
     Baid         INTEGER PRIMARY KEY,
     username     TEXT not null,
     passwordHash TEXT not null,
-    customTitleOn INTEGER NOT NULL DEFAULT 0
+    customTitleOn INTEGER NOT NULL DEFAULT 1
 );
