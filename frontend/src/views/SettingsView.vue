58bfccca50fb4f18d85993edfe53f5838d00eaa8
sohankolla <sohan.kolla03@gmail.com>
2024-08-26 01:44:41 -0400

    feature<frontend>: Completed Settings page by adding access code management data table

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,23 +1,49 @@
 <script setup lang="ts">
 import {useAuthStore} from "@/store/authStore";
 import {useToast} from "vue-toastification";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 const authStore = useAuthStore();
 const toast = useToast();
 
 const username = ref(authStore.username);
+watch(() => authStore.initialized, (value) => username.value = authStore.username);
 
 const currentPassword = ref('');
 const newPassword = ref('');
 const confirmNewPassword = ref('');
 
-//const accessCodes = ref(await fetch(`/api/user/${authStore.baid}/access_codes`));
+// loading access codes with a get request
+const accessCodes = ref<string[]>([]);
+const fetchAccessCodes = async () => {
+  try {
+    const response = await fetch(`/api/user/${authStore.baid}/access_codes`);
+    if (response.ok) {
+      accessCodes.value = await response.json();
+    } else {
+      toast.error("Failed to fetch access codes");
+      console.error('Unexpected access code data response status:', response.status);
+    }
+  } catch (error) {
+    toast.error("Failed to fetch access codes");
+    console.error('Failed to fetch access codes:', error);
+  }
+}
+// calling the above function when baid loads
+if (authStore.initialized) {
+  fetchAccessCodes()
+} else {
+  watch(() => authStore.initialized, (value) => {
+    fetchAccessCodes();
+  });
+}
+
 const newAccessCode = ref('');
+
 
 async function changeUsername(): Promise<boolean> {
   const data = {
-    username: username
+    username: username.value
   }
 
   const response = await fetch(`/auth/user/${authStore.baid}/username`, {
@@ -38,6 +64,7 @@ async function changeUsername(): Promise<boolean> {
     return false;
   }
 }
+
 
 async function changePassword(): Promise<boolean> {
   const data = {
@@ -61,6 +88,9 @@ async function changePassword(): Promise<boolean> {
 
   if (response.ok) {
     toast.success('Password Changed Successfully');
+    currentPassword.value = '';
+    newPassword.value = '';
+    confirmNewPassword.value = '';
     return true;
   } else {
     toast.error(`Failed to change password: ${await response.text()}`);
@@ -68,60 +98,55 @@ async function changePassword(): Promise<boolean> {
   }
 }
 
-// async function addAccessCode(): Promise<boolean> {
-//   const data = {
-//     newAccessCode: newAccessCode.value
-//   }
-//
-//   const response = await fetch(`/api/user/${authStore.baid}/access_codes`, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(data),
-//   });
-//
-//
-//   if (response.ok) {
-//     toast.success('Access Codes Updated Successfully');
-//     accessCodes =
-//     return true;
-//   } else {
-//     toast.error(`Failed to update access codes: ${await response.text()}`);
-//     return false;
-//   }
-// }
-//
-// async function removeAccessCode(): Promise<boolean> {
-//   const data = {
-// //    accessCodes: accessCodes
-//   }
-//
-//   // MAKE SURE ON THE BACKEND THAT WHEN THE USER TRIES TO DELETE SOME ACCESS CODE
-//   // THAT THE ACCESS CODE ACTUALLY BELONGS TO THEM, OTHERWISE ANY LOGGED IN EGTS USER
-//   // CAN SEND A POSTMAN REQUEST TO DELETE AN ACCESS CODE
-//
-//   // ALSO MAKE SURE ON THE BACKEND THAT THE USER HAS ATLEAST ONE ACCESS CODE
-//   // SO THEY CANT DELETE IT
-//
-//   const response = await fetch(`/api/user/${authStore.baid}/access_codes`, {
-//     method: 'DELETE',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(data),
-//   });
-//
-//
-//   if (response.ok) {
-//     toast.success('Access Codes Updated Successfully');
-//     accessCodes =
-//     return true;
-//   } else {
-//     toast.error(`Failed to update access codes: ${await response.text()}`);
-//     return false;
-//   }
-// }
+
+async function addAccessCode(): Promise<boolean> {
+  const data = {
+    accessCode: newAccessCode.value
+  }
+
+  const response = await fetch(`/api/user/${authStore.baid}/access_codes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+
+  if (response.ok) {
+    toast.success('Access Code Added Successfully');
+    accessCodes.value.push(newAccessCode.value);
+    newAccessCode.value = '';
+    return true;
+  } else {
+    toast.error(`Failed to add access code: ${await response.text()}`);
+    return false;
+  }
+}
+
+async function deleteAccessCode(accessCode: string): Promise<boolean> {
+  const data = {
+    accessCode: accessCode
+  }
+
+  const response = await fetch(`/api/user/${authStore.baid}/access_codes`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+
+  if (response.ok) {
+    toast.success('Access Code Deleted Successfully');
+    accessCodes.value = accessCodes.value.filter(currentCode => currentCode != accessCode)
+    return true;
+  } else {
+    toast.error(`Failed to delete access code: ${await response.text()}`);
+    return false;
+  }
+}
 
 </script>
 
@@ -144,6 +169,7 @@ async function changePassword(): Promise<boolean> {
                   minlength="1"
                   maxlength="20"
                   v-model="username"
+                  @keydown.enter.prevent
               >
             </div>
 
@@ -167,6 +193,7 @@ async function changePassword(): Promise<boolean> {
                   required
                   minlength="1"
                   v-model="currentPassword"
+                  @keydown.enter.prevent
               >
             </div>
             <div class="flex flex-col border-b border-cl6 px-2 mx-2">
@@ -180,6 +207,7 @@ async function changePassword(): Promise<boolean> {
                   minlength="8"
                   maxlength="100"
                   v-model="newPassword"
+                  @keydown.enter.prevent
               >
             </div>
             <div class="flex flex-col border-b border-cl6 px-2 mx-2">
@@ -193,6 +221,7 @@ async function changePassword(): Promise<boolean> {
                   minlength="8"
                   maxlength="100"
                   v-model="confirmNewPassword"
+                  @keydown.enter.prevent
               >
             </div>
 
@@ -205,32 +234,57 @@ async function changePassword(): Promise<boolean> {
           </form>
         </div>
 
-        <form class="flex flex-col mx-2 space-y-2 border-2 rounded-xl w-full md:w-2/3">
-          <div class="flex flex-col border-b border-cl6 pt-2 px-2 mx-2 mb-2">
-            <h1 class="text-2xl text-center font-bold mb-2">Access Codes</h1>
-            <label for="newAccessCode" class="text-sm mb-1">Add Access Code:</label>
-            <input
-                class="appearance-none bg-transparent border-none text-gray-700 focus:outline-none w-full h-10"
-                type="text"
-                placeholder="New Access Code"
-                id="newAccessCode"
-                required
-                minlength="1"
-                maxlength="100"
-                v-model="newAccessCode"
+        <div class="flex flex-col mx-2 space-y-6 border-2 rounded-xl w-full md:w-2/3">
+          <form @submit.prevent="addAccessCode" class="flex flex-col space-y-2 w-full">
+            <div class="flex flex-col border-b border-cl6 pt-2 px-2 mx-2 mb-2">
+              <h1 class="text-2xl text-center font-bold mb-2">Manage Access Codes</h1>
+              <label for="newAccessCode" class="text-sm mb-1">Add Access Code:</label>
+              <input
+                  class="appearance-none bg-transparent border-none text-gray-700 focus:outline-none w-full h-10"
+                  type="text"
+                  placeholder="New Access Code"
+                  id="newAccessCode"
+                  required
+                  minlength="1"
+                  maxlength="100"
+                  v-model="newAccessCode"
+                  @keydown.enter.prevent
+              >
+            </div>
+            <button
+                class="w-1/4 self-center p-2 text-base font-bold border-cl6 bg-cl6 text-cl5 rounded-lg border-2 shadow hover:bg-opacity-80 hover:transition-all"
+                type="submit"
             >
-          </div>
-          <button
-              class="w-1/4 self-center p-2 text-base font-bold border-cl6 bg-cl6 text-cl5 rounded-lg border-2 shadow hover:bg-opacity-80 hover:transition-all"
-              type="submit"
+              Add
+            </button>
+          </form>
+          <v-data-table
+              class="h-full"
+              :items="accessCodes.map((code, index) => ({ id: index + 1, code }))"
+              :headers="[
+                      { title: 'Active Codes', align: 'start', value: 'code', sortable: false },
+                      { title: '', align: 'end', value: 'actions', sortable: false }
+                      ]"
+              item-key="id"
+              item-value="code"
+              no-data-text="No access codes found"
+              items-per-page="5"
+              density="comfortable"
+              :items-per-page-options="[{value: 5, title: '5'}]"
           >
-            Add
-          </button>
-        </form>
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                  size="small"
+                  icon="mdi-delete"
+                  @click="deleteAccessCode(item.code)"
+              >
+              </v-icon>
+            </template>
+          </v-data-table>
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <style scoped>
